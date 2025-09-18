e6568b054b0389acc5cfa540cc1e0bd4042a8c22
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-09-18 19:21:26 +0200

    fixed route and db issues

--- laravel10-template/app/Models/User.php
+++ laravel10-template/app/Models/User.php
@@ -12,16 +12,22 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+
+    public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    'name',
+    'last_name',
+    'username',
+    'email',
+    'password',
+    'role_id',
+];
 
     /**
      * The attributes that should be hidden for serialization.
