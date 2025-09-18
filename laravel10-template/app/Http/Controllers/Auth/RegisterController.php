cab4cac1acc6cad4386a52d582db3f5ffd7ec98d
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-09-18 19:32:39 +0200

    another db refactoring

--- laravel10-template/app/Http/Controllers/Auth/RegisterController.php
+++ laravel10-template/app/Http/Controllers/Auth/RegisterController.php
@@ -63,11 +63,13 @@ class RegisterController extends Controller
      * @return \App\Models\User
      */
     protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+   {
+    return User::create([
+        'name' => $data['name'],
+        'last_name' => $data['last_name'] ?? null,
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+        'role_id' => 1,
+    ]);
     }
 }
